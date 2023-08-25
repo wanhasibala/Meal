0bcbe03ae09ffff1a96b65e1dd0dc4d1a18dd4a8
wanhasibala <wanhasibal1122@gmail.com>
2023-08-25 17:12:59 +0700

    styling mealItem

--- components/MealItem.js
+++ components/MealItem.js
@@ -1,9 +1,48 @@
-import { Text, View } from "react-native"
+import { StyleSheet } from "react-native";
+import { Image, Pressable, Text, View } from "react-native";
 
-function MealItem({title}){
-    return <View>
-        <Text>{title}</Text>
+function MealItem({ title, imageUrl, duration, complexity, affordability }) {
+  return (
+    <View style={styles.mealItem}>
+      <Pressable>
+        <View >
+          <Image source={{uri: imageUrl}} style={styles.image}/>
+          <Text style={styles.title} >{title}</Text>
+        </View>
+        <View style={styles.details}>
+            <Text style={styles.detailItem} >{duration}m</Text>
+            <Text style={styles.detailItem} >{complexity.toUpperCase()}</Text>
+            <Text style={styles.detailItem} > {affordability.toUpperCase()}</Text>
+        </View>
+      </Pressable>
     </View>
+  );
 }
 
-export default MealItem
+export default MealItem;
+const styles= StyleSheet.create({
+    mealItem:{
+        margin: 16,
+        borderRadius: 8,
+        overflow: 'hidden',
+        backgroundColor: 'white'
+    },
+    image:{
+        width: '100%',
+        height: 200,
+    },
+    title: {
+        fontWeight: 'bold',
+        textAlign: 'center',
+        fontSize: 18,
+        padding: 4,
+    },
+    details:{
+        flexDirection: 'row',
+        padding: 8,
+        justifyContent: 'center'
+    },
+    detailItem: {
+        marginHorizontal: 4,
+    }
+})
--- screens/MealOverviewScreen.js
+++ screens/MealOverviewScreen.js
@@ -9,12 +9,28 @@ function MealOverviewScreen({ route }) {
     return mealItem.categoryIds.indexOf(catId) >= 0;
   });
 
-  function renderMealItem(itemData){
-    return <MealItem title={itemData.item.title}/>
+  function renderMealItem(itemData) {
+    const item= itemData.item;
+
+    const mealItemProps= {
+        title: item.title,
+        imageUrl: item.imageUrl,
+        duration: item.duration,
+        complexity: item.complexity,
+        affordability: item.affordability,
+    }
+
+    return (
+      <MealItem {...mealItemProps} />
+    );
   }
   return (
     <View style={styles.container}>
-        <FlatList data={displayedMeals} keyExtractor={(item)=>item.id} renderItem={renderMealItem} />
+      <FlatList
+        data={displayedMeals}
+        keyExtractor={(item) => item.id}
+        renderItem={renderMealItem}
+      />
     </View>
   );
 }
